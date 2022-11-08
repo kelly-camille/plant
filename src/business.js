4e7be287f386004a94e3ff149cee0d35ceeb2e21
Kelly Bruce <kelly.bruce.cs@gmail.com>
2022-11-08 14:39:44 -0800

    change state function Co-authored-by: Zachary Waggoner <zacwaggoner42@gmail.com>

--- src/business.js
+++ src/business.js
@@ -1,9 +1,175 @@
-// const changePlantState = (plant, property) => {
+// reference code
+
+
+// Business Logic
+class Plant {
+  constructor() {
+    this.water = 0;
+    this.soil = 0;
+    this.light = 0;
+    this.hp = 10;
+  }
+}
+
+// This function stores our state.
+export const storeState = () => {
+  let currentState = {};
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  };
+};
+
+export const stateControl = storeState();
+
+// This is a function factory. 
+// We can easily create more specific functions that 
+// alter a plant's soil, water, and light to varying degrees.
+export const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value,
+    });
+  };
+};
+
+// We create four functions using our function factory. 
+// We could easily create many more.
+// give food +
+const feed = changeState("soil")(1);
+const blueFood = changeState("soil")(5);
+
+//give water +
+const hydrate = changeState("water")(1);
+const superWater = changeState("water")(5);
+
+// give light +
+const light = changeState("light")(1);
+const sunLight = changeState("light")(5);
+
+// give HP +
+const potion = changeState("hp")(1);
+const powerPotion = changeState("hp")(5);
+
+// damage -
+const dandelionSmash = changeState("hp")(-2);
+const venusFlykick = changeState("hp")(-4);
+
+// Damage over time
+const removeSoil = changeState("soil")(-1);
+// function removeSoil(){
+//   console.log("removed soil");
+//   changeState("soil")(-1);
+// } 
+const removeWater = changeState("water")(-1);
+// function removeWater(){
+//   console.log("removed water");
+//   changeState("water")(-1);
+// } 
+const removeHP = changeState("hp")(-1);
+// function removeHP(){
+//   console.log("removed hp");
+//   changeState("hp")(-1);
+// }
+
+const removeLight = changeState("light")(-1);
+
+
+//init
+const initSoil = changeState("soil")(0);
+const initWater = changeState("water")(0);
+const initLight = changeState("light")(0);
+const initHP = changeState("hp")(10);
+
+function setTimer(){
+  setInterval(() => stateControl(removeSoil), 1000);
+  setInterval(() => stateControl(removeWater), 1000);
+  setInterval(() => stateControl(removeLight), 1000); 
+  setInterval(() => stateControl(removeHP), 1000);
+  setInterval(() => update(), 1000);
+}
+
+function init(){
+  stateControl(initSoil);
+  stateControl(initWater);
+  stateControl(initLight);
+  stateControl(initHP);
+  const currentState = stateControl();
+  document.getElementById('soil-value').innerText = `Soil: ${currentState.soil}`;
+  document.getElementById('water-value').innerText = `Water: ${currentState.water}`;
+  document.getElementById('light-value').innerText = `Light: ${currentState.light}`;
+  document.getElementById('hp-value').innerText = `HP: ${currentState.hp}`;
+}
+
+// UI Logic
+window.onload = function() {
+  // This function has side effects because we are manipulating the DOM.
+  // Manipulating the DOM will always be a side effect. 
+  // Note that we only use one of our functions to alter soil. 
+  // You can easily add more.
+  document.getElementById('feed').onclick = function() {
+    const newState = stateControl(blueFood);
+    document.getElementById('soil-value').innerText = `Soil: ${newState.soil}`;
+  };
+  document.getElementById('water').onclick = function() {
+    const newState = stateControl(superWater);
+    document.getElementById('water-value').innerText = `Water: ${newState.water}`;
+  };
+  document.getElementById('giveLight').onclick = function() {
+    const newState = stateControl(sunLight);
+    document.getElementById('light-value').innerText = `Light: ${newState.light}`;
+  };
+  document.getElementById('damage').onclick = function() {
+    const newState = stateControl(venusFlykick);
+    document.getElementById('hp-value').innerText = `HP: ${newState.hp}`;
+  };
+  document.getElementById('init').onclick = function() {
+    init();
+    setTimer();
+    document.getElementById('init').className = "hidden";
+  };
+
+  // This function doesn't actually do anything useful in this application 
+  // — it just demonstrates how we can "look" at the current state 
+  // (which the DOM is holding anyway). 
+  // However, students often do need the ability to see the current state 
+  // without changing it so it's included here for reference.
+  document.getElementById('show-state').onclick = function() {
+    // We just need to call stateControl() without arguments 
+    // to see our current state.
+    update();
+  };
+};
+
+function update(){
+  const currentState = stateControl();
+  document.getElementById('soil-value').innerText = `Soil: ${currentState.soil}`;
+  document.getElementById('water-value').innerText = `Water: ${currentState.water}`;
+  document.getElementById('light-value').innerText = `Light: ${currentState.light}`;
+  document.getElementById('hp-value').innerText = `HP: ${currentState.hp}`;
+}
+
+
+// const hydrate = (plant) => {
 //   return {
 //     ...plant,
-//     [property]: (plant[property] || 0) + 1
+//     water: (plant.water || 0) + 1
 //   }
 // }
 
-// state meaning object
-// prop meaning property of object
+// const feed = (plant) => {
+//   return {
+//     ...plant,
+//     soil: (plant.soil || 0) + 1
+//   }
+// }
+//   this.soil ++
+
+// const giveLight = (plant) => {
+//   return {
+//     ...plant,
+//     light: (plant.light || 0) + 1
+//   }
+// }
--- src/plant.js
+++ src/plant.js
@@ -1,14 +1,22 @@
-const storeState = () => {
-  let currentState = {};
-  return (stateChangingFunction) => {
-    const newState = stateChangingFunction(currentState);
-    currentState = {...newState};
+//State - anything we are asking a computer to remember. Defines the current conditions of an application
+
+// This function stores our state
+const storeState = () => { // outer function
+  let currentState = {}; // will be modified
+  return (stateChangingFunction = state => state) => { // inner function that takes another function as an argument -- will specify the exact change that should be made to currentState
+    const newState = stateChangingFunction(currentState); //call the function on currentState and save it to newState
+    currentState = {...newState}; // makes a copy of newState and saves it as currentState
     return newState;
   }
 }
 
-const stateControl = storeState();
+const stateControl = storeState(); // invoking storeState() function and creating a closure over the currentStatevariable. ^^
+// holds the inner function and retains the currentState variable from the outer function. 
+// 
 
+
+//This is a function factory
+// used to create more specific functions taht alter a plant's soil water and light
 const changeState = (prop) => {
   return (value) => {
     return (state) => ({
@@ -18,17 +26,25 @@ const changeState = (prop) => {
   }
 };
 
-const feed = changeState("soil");
-const hydrate = changeState("water");
-const giveLight = changeState("light");
+// create our functions using ^^ function factory
 
+// give food
+const feed = changeState("soil");
 const blueFood = feed(5);
 const greenFood = feed(10);
 const yuckyFood = feed(-5);
-changeState("soil")(5)(plant);
 
-feed(5)(plant);
+const hydrate = changeState("water");
+const giveLight = changeState("light");
 
-blueFood(plant);
-module.exports = {changeState, feed, hydrate, giveLight};
+
+// changeState("soil")(5)(plant);
+// const fedPlant = stateControl(blueFood);
+// const plantFedAgain = stateControl(greenFood);
+// console.log(fedPlant);
+
+// feed(5)(plant);
+
+// blueFood(plant);
+ module.exports = {changeState, feed, hydrate, giveLight, stateControl, blueFood, greenFood};
 
